48e38ffdafd3907dd4fc76255a60ebd2a179806f
Hendi Santika <hendisantika@gmail.com>
2018-04-11 21:38:39 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/useraccountregistration/UserAccountRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.useraccountregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAccountRegistrationApplication.class, args);
+    }
+}
